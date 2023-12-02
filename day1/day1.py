bf5935306cd03297190c2334e3e7dbfbeb76025a
Thomas Chardo <chardonnens.thomas@protonmail.com>
2023-12-01 18:03:40 -0800

    day 1

---
+++ day1/day1.py
@@ -0,0 +1,40 @@
+import re
+
+# 1 star
+
+with open("input.txt", "r") as f:
+    lines = f.readlines()
+    lines = [line.strip() for line in lines]
+
+sum = 0
+for line in lines:
+    line = re.sub("[^0-9]", "", line)
+    res = line[0] + line[-1]
+    sum += int(res)
+
+print(sum)
+
+# 2 stars
+
+sum = 0
+numbers_in_letters = {
+    "one": "1",
+    "two": "2",
+    "three": "3",
+    "four": "4",
+    "five": "5",
+    "six": "6",
+    "seven": "7",
+    "eight": "8",
+    "nine": "9"
+}
+for line in lines:
+    for key in numbers_in_letters.keys():
+        if key in line:
+            line = line.replace(key, numbers_in_letters[key])
+    line = re.sub("[^0-9]", "", line)
+    res = line[0] + line[-1]
+    sum += int(res)
+
+print(sum)
+
