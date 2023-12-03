2f580b294156e6868b8610ed959319bd3f41c17a
Thomas Chardo <chardonnens.thomas@protonmail.com>
2023-12-02 19:40:44 -0800

    day 2, attempt 2 stars

--- day2/day2.py
+++ day2/day2.py
@@ -50,8 +50,55 @@ for line in lines:
     res.append(int(game))
   
 print(sum(res))
+
+## Part 2
         
 
+with open("input.txt", "r") as f:
+    lines = f.readlines()
+    lines = [line.strip() for line in lines]
 
+res = []
 
+possible_game = {
+    "red": 12,
+    "green": 13,
+    "blue": 14
+  }
 
+# each line is a new game with its throws
+for line in lines:
+
+  # split the line into the game number and the rest
+  game = line.split(":")[0]
+  game = game.split(" ")[1] # Game id
+  rest = line.split(":")[1] # List of throws
+
+  # split the rest into the different throws
+  throws = rest.split(";")
+
+  # make a dictionnary out of the throws
+  game_dict = {
+    "red": 20000,
+    "green": 20000,
+    "blue": 20000
+  }
+
+  for throw in throws:
+    # split the throw into the color and the number
+    pairs = throw.split(",")
+    pairs = [pair.strip() for pair in pairs]
+    for pair in pairs:
+      color = pair.split(" ")[1]
+      number = int(pair.split(" ")[0])
+      if number < game_dict[color]:
+        game_dict[color] = number
+    
+    game_score = 1
+    for color in game_dict.keys():
+      game_score = game_score * game_dict[color]
+    print(game_score)
+  res.append(int(game_score))
+  
+print(sum(res))
+        
